73e0e491c82f2216f43dee0670609e0b23fb9844
paulcalcraft@hotmail.co.uk <paulcalcraft@hotmail.co.uk@80fd79d2-fda8-11dd-bc25-5f29055a2a2d>
2009-05-13 11:37:07 +0000

    Created test code for italics and bold combined. Added ItalicsTagRenderer to the list of renderers in Renderer class.

---  browsermonkey/Programming Team/scratch/BrowserMonkey/src/browsermonkey/render/ItalicsTagRenderer.java
+++  browsermonkey/Programming Team/scratch/BrowserMonkey/src/browsermonkey/render/ItalicsTagRenderer.java
@@ -10,7 +10,6 @@ import java.util.*;
  * @author Daniel Cooper dc92
  */
 public class ItalicsTagRenderer extends TagRenderer {
-
     @Override
     public void render(Renderer renderer, TagDocumentNode tag, LayoutRenderNode parent, Map<Attribute, Object> formatting) {
         Map<Attribute, Object> newFormatting = (Map<Attribute, Object>)((Hashtable)formatting).clone();
